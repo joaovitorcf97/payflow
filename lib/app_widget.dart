cc35e40f39632b14490b555dd0a197c8ad98cfdb
João Vitor <joaovitorcfdev@gmail.com>
2021-08-17 00:03:11 -0300

    Adicionando bottomNavigationBar

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
 
-import 'modules/home/home_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Pay Flow',
       theme: ThemeData(
         primaryColor: AppColors.primary,
       ),
-      home: HomePage(),
+      initialRoute: "/splash",
+      routes: {
+        "/splash": (context) => SplashPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
---
+++ lib/shared/models/user_model.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+class UserModel {
+  final String name;
+  final String? photoURL;
+
+  UserModel({
+    required this.name,
+    required this.photoURL,
+  });
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(name: map['name'], photoURL: map['photoURL']);
+  }
+
+  factory UserModel.fromJson(String json) =>
+      UserModel.fromMap(jsonDecode(json));
+
+  Map<String, dynamic> topMap() => {
+        "name": name,
+        "photoURL": photoURL,
+      };
+
+  String toJson() => jsonEncode(topMap());
+}
